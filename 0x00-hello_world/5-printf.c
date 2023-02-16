32aaa0227e484ad979f75f2d49fb0ee4781df94a
Remone Jacobs <jacobsremone@gmail.com>
2023-02-16 14:54:47 -0800

    Task5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Program that prints a line using printf
+ * return: ALways 0
+ */
+int main(void)
+{
+	pintf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
