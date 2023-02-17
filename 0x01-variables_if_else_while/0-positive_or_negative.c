7c30e5e25af2035f49f10c9c7186f9467a2295a6
Remone Jacobs <jacobsremone@gmail.com>
2023-02-17 04:22:19 -0800

    Task 0

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n;
+	if (n > 0)
+	{
+		printf("n is bigger than 0\n");
+	}
+	else if (n == 0)
+	{
+		printf("n is equal to 0\n");
+	}
+	else
+	{
+		printf("n is smaller than 0\n");
+	}
+	return (0);
+}
