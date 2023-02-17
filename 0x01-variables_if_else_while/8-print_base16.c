b316fa5b9b393b28458547b903f2dd4d5f0fbd3e
Remone Jacobs <jacobsremone@gmail.com>
2023-02-17 12:53:24 -0800

    Task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n;
+
+	char ch;
+
+	for (n = 0; n < 10; n++)
+	{
+		putchar(n);
+	}
+	for (ch = 'a'; ch > 'g'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
