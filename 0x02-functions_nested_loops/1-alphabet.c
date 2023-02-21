2e1dc0383f6353de9141bf2d010b33bf8718fbae
Remone Jacobs <jacobsremone@gmail.com>
2023-02-21 10:25:09 -0800

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+		_putchar('\n');
+	}
+}
