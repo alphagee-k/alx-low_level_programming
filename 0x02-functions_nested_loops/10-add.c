ba02ceac746215377159c3042e18d7e010c22c2e
Remone Jacobs <jacobsremone@gmail.com>
2023-02-21 16:51:05 -0800

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - adds two integers
+ * @a: First number to be computed
+ * @b: Second number to be computed
+ * Return: Integer value
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
