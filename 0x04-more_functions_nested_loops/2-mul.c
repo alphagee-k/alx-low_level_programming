4224c34d77d6c32a8d7be956881464979c5f45d6
Remone Jacobs <jacobsremone@gmail.com>
2023-02-23 11:34:29 -0800

    Task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - multiplies two integers
+ * @a: first integer being checked
+ * @b: second integer being checked
+ * Return: ...
+ */
+int mul(int a, int b)
+{
+	int mult;
+
+	mult = a * b;
+
+	_putchar(mult);
+}
