218b8e6bfbd28ce0074d05b86d92c117040aee45
Remone Jacobs <jacobsremone@gmail.com>
2023-02-23 11:07:36 -0800

    Task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isupper - checks for uppercase character
+ * @c: The integer being checked
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+int _isupper(int c)
+{
+	if (c > 64 && c < 91)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
