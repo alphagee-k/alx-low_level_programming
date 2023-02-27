5eaa641ca9acc6f769aeb058e642b416cc38977a
Remone Jacobs <jacobsremone@gmail.com>
2023-02-27 14:45:18 -0800

    Task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_rev - imprime en reversa
+ * @s: string
+ * return: 0
+ */
+void print_rev(char *s)
+{
+	int l = 0;
+	int a;
+
+	while (*s != '\0')
+	{
+		l++;
+		s++;
+	}
+	s--;
+	for (a = l; a > 0; a--)
+	{
+		_putchar(*s);
+		s--;
+	}
+
+	_putchar('\n');
+}
