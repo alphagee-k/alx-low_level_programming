53a1e0978641557d0e8299da7ab93a7477ab2d34
Remone Jacobs <jacobsremone@gmail.com>
2023-03-02 03:06:42 -0800

    Task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strcmp - function that compares two strings
+ * @s1: First string
+ * @s2: second string
+ * Return: s1[a] - s2[a]
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int a = 0;
+
+	while (s1[a] != '\0' && s2[a] != '\0')
+	{
+		if (s1[a] != s2[a])
+		{
+			return (s1[a] - s2[a]);
+		}
+		a++;
+	}
+	return (0);
+}
