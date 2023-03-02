6fe6da0e9bd53a63adb5d4e6faed6c927b429027
Remone Jacobs <jacobsremone@gmail.com>
2023-03-02 02:47:20 -0800

    Task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * char *_strncpy - function that copies a string
+ * @dest: first string
+ * @src: second string
+ * @n: integer
+ * Return: string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int a = 0;
+
+	while (src[a] != '\0' && (a < n))
+	{
+		dest[a] = src [a];
+		a++;
+	}
+	while (a < n)
+	{
+		dest[a] = '\0';
+		a++;
+	}
+	return (dest);
+}
