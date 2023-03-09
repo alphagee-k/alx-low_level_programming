139f55305be9a546465c5e02c1b03a94bf112eca
Remone Jacobs <jacobsremone@gmail.com>
2023-03-09 06:44:46 -0800

    Task 5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _sqrt_recursion -  function that returns the natural square root of a number
+ * @n: integer being tested
+ * @x: int being tested
+ * Return: 0
+ */
+int _sqrt_recursion(int n, int x)
+{
+	int x;
+
+	if (x * x > n)
+		return (-1);
+	if (x * x == n)
+		return (x);
+	return (_sqrt_recursion(n, i + 1));
+}
