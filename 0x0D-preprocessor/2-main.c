747a680e80602d828fb9fd55e78f0b88c5c578e9
root <Remone Jacobs>
2023-03-22 12:14:16 -0700

    Task 2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - program that prints the name of the file it was compiled from, followed by a new line
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("%s\n", _FILE_);
+	return (0);
+}
