aab5c627ae7fddeb2fbd033ef28a245b92d86976
root <Remone Jacobs>
2023-03-23 16:23:01 -0700

    task 3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "3-calc.h"
 /**
  * main - Prints the result of simple operations.
@@ -15,7 +16,7 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 	if (argc != 4)
 	{
 		printf("Error\n");
-		exit(98);
+		exit (98);
 	}
 
 	num1 = atoi(argv[1]);
@@ -25,14 +26,14 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
 		printf("Error\n");
-		exit(99);
+		exit (99);
 	}
 
 	if ((*op == '/' && num2 == 0) ||
 	    (*op == '%' && num2 == 0))
 	{
 		printf("Error\n");
-		exit(100);
+		exit (100);
 	}
 
 	printf("%d\n", get_op_func(op)(num1, num2));
