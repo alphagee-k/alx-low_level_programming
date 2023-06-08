b4a242c11bb45e588f3944dbc796cc2d332a5b4c
Remone Jacobs <jacobsremone@gmail.com>
2023-06-08 14:26:02 -0700

    Task 4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _pow_recursion - returns x to teh power of y
+ * @x: base
+ * @y: power
+ * Return: 0;
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y = 0)
+		return (1);
+	return (x * _pow_recursion(x,y - 1));
+}
