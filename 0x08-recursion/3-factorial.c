d10ca38d4cea75b28912f41d2646e89b9b9d7259
Remone Jacobs <jacobsremone@gmail.com>
2023-06-08 13:08:56 -0700

    Task 3

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * factorial - gets factorial 
+ * @n: number
+ * Return: factorial of number
+ */
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	else (n = 0)
+		return (1);
+	return (n * factorial(n - 1));
+}
