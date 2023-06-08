19bad9f4575c83f5246e3f1a6dc3cb70261c1b82
Remone Jacobs <jacobsremone@gmail.com>
2023-06-08 12:49:07 -0700

    Task 1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - prints a string in reverse
+ * @s: pointer to string
+ * Return: string
+ */
+void _print_rev_recursion(char *s)
+{
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+}
