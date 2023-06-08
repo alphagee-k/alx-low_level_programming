eb67b19ea00edda61a865bff96c88ab4c4b69930
Remone Jacobs <jacobsremone@gmail.com>
2023-06-08 12:57:23 -0700

    Task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen_recursion - check length of a string
+ * @s: pointer to string
+ * Return: length of a string
+ */
+int _strlen_recursion(char *s)
+{
+	int n;
+
+	if (*s)
+	{
+		n++;
+		n = n + _strlen_recursion(s + 1);
+	}
+	return n;
+}
